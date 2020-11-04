167762f8017607774e436e5bb3f141c2c2b60764
MB <bergerm613@gmail.com>
2020-11-03 21:30:11 -0500

    setting up cell

--- src/main/java/touro/GameOfLife/Grid.java
+++ src/main/java/touro/GameOfLife/Grid.java
@@ -1,4 +1,15 @@
 package touro.GameOfLife;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grid {
+    public static final int HEIGHT = 40;
+    public static final int WIDTH = 100;
+
+    private final List<Cell> cells = new ArrayList<>();
+
+    public List<Cell> getCells() {
+        return cells;
+    }
 }
